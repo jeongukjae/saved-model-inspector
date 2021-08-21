7d9c87c6e7100e65cf668abbe3ab80fd4c9f329f
Jeong Ukjae <JeongUkJae@gmail.com>
2021-08-21 19:17:42 +0900

    add example link

--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -3,6 +3,7 @@ import Box from "@material-ui/core/Box"
 import { useState } from "react"
 import { SavedModel } from "../proto/tensorflow/core/protobuf/saved_model_pb"
 import { makeStyles } from "@material-ui/core/styles"
+import { Typography } from "@material-ui/core"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,6 +37,19 @@ export default function FileUploader({ onRead, handleReadingState }) {
         Inspect saved_model.pb
         <input type="file" hidden={true} onChange={(e) => handleFileChosen(e.target.files[0])} accept=".pb" />
       </Button>
+      <Box m={1}>
+        <Typography>
+          Example <code>saved_model.pb</code>
+          <br />
+          <a href="https://github.com/jeongukjae/saved-model-inspector/raw/main/testdata/bert_en_uncased_preprocess_3-saved_model.pb">
+            <code>bert_en_uncased_preprocess</code>
+          </a>
+          <br />
+          <a href="https://github.com/jeongukjae/saved-model-inspector/raw/main/testdata/small_bert_bert_en_uncased_L-2_H-128_A-2_2-saved_model.pb">
+            <code>small_bert_bert_en_uncased_L-2_H-128_A-2</code>
+          </a>
+        </Typography>
+      </Box>
     </Box>
   )
 }
