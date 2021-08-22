73d21b1acde37d73b0d1c51cce3a74ec909f834f
Jeong Ukjae <JeongUkJae@gmail.com>
2021-08-22 16:52:45 +0900

    fix button angle

--- src/components/Inspector.js
+++ src/components/Inspector.js
@@ -180,12 +180,12 @@ export default function Inspector({ rawSavedModelPb }) {
 
 const useToggleStyle = makeStyles((theme) => ({
   expand: {
-    transform: "rotate(180deg)",
+    transform: "rotate(0deg)",
     marginLeft: "auto",
     transition: theme.transitions.create("transform", { duration: theme.transitions.duration.shortest }),
   },
   expandOpen: {
-    transform: "rotate(0deg)",
+    transform: "rotate(180deg)",
   },
 }))
 
